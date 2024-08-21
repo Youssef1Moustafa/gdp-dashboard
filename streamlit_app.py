d8138a8266dccafc32d49f736cc0602d97befc24
Youssef Moustafa <109866940+Youssef1Moustafa@users.noreply.github.com>
2024-08-21 23:19:26 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,5 +8,5 @@ fuelconsumption = st.number_input('FUELCONSUMPTION_CITY',min_value =0, max_value
 with open('model.pkl', 'rb' ) as file:
   model = pickle.load(file)
 output = model.predict([[engine_size, cylinder, fuelconsumption]])
-st.write('co2 of car is :', output[0][0])
+st.write('co2 of car is :', output[0][0]) 
 
