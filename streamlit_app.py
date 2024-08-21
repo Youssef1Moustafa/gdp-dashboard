3db24638a0ddd6a8bb2801db62d63c209dded62a
Youssef Moustafa <109866940+Youssef1Moustafa@users.noreply.github.com>
2024-08-21 22:50:57 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit as st
 st.title('model')
-st.number_input('engine_size',min_value =0, max_value = 10, value = 1)
+engine_size = st.number_input('engine_size',min_value =0, max_value = 10, value = 1)
+cylinder = st.number_input('CYLINDERS',min_value =0, max_value = 10, value = 1)
+fuelconsumption = st.number_input('FUELCONSUMPTION_CITY',min_value =0, max_value = 10, value = 1)
+
+output = model.predict([[engine_size, cylinder, fuelconsumption]])
+st.write('co2 of car is :', output[0][0])
